c03e6a76d026623a74906651fc0063c0593d0a2a
Muturi403 <faith.muturi@student.moringaschool.com>
2021-05-03 09:37:40 +0300

    initial commit of the project

---
+++ sheet.js
@@ -0,0 +1,51 @@
+var shadeA = 510;
+shadeA *= 45;
+
+console.log(shadeA);
+
+var shadeB = 308;
+shadeB *= 45;
+
+console.log(shadeB);
+
+var shadeC = 486;
+shadeC *= 45;
+
+console.log(shadeC);
+
+var shadeD = 572;
+shadeD *= 45;
+
+console.log(shadeD);
+
+//var shadeA, shadeB, shadeC, shadeD ;
+//shadeA = 22950;
+//shadeB = 13860;
+//shadeC = 21870;
+//shadeD = 25740;
+//console.log(shadeA+shadeB+shadeC+shadeD);
+
+var totalProduction = 1876;
+console.log(totalProduction);
+
+var weeklyIncome = 590940;
+console.log(weeklyIncome);
+
+var yearlyIncome = 30813300;
+console.log(yearlyIncome);
+
+var eachMonthlyIncome = {
+    january: 2617020,
+    february: 2448180,
+    march: 2617020,
+    april:2532600,
+    may:2617020,
+    june:2532600,
+    july:2617020,
+    august: 2617020,
+    september: 2532600,
+    october: 2617020,
+    november: 2532600,
+    december: 2617020
+};
+console.log(eachMonthlyIncome);
